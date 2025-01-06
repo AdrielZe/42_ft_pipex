a3193a2da88b299b28bb96a0301bf4e64979ce3e
Adriel Silveira De Oliveira <asilveir@student.42sp.org.br>
2025-01-06 10:38:08 -0300

    pipex completed

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: asilveir <asilveir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 19:14:00 by asilveir          #+#    #+#             */
-/*   Updated: 2024/12/17 18:33:53 by asilveir         ###   ########.fr       */
+/*   Updated: 2025/01/06 10:35:04 by asilveir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@
 # include <stdlib.h>
 # include "../libft/libft.h"
 
+void	execute_command(char *argv, char **envp);
+void	child_proccess(int *fd, char **argv, char **envp);
+void	parent_proccess(int *fd, char **argv, char **envp);
+void	args_verification(int argc);
+void	free_paths(char **paths);
+int		search_for_path_index(char **envp);
 char	*search_valid_path(char *cmd, char **envp);
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,13 +6,13 @@
 /*   By: asilveir <asilveir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 18:32:34 by asilveir          #+#    #+#             */
-/*   Updated: 2024/12/17 18:58:42 by asilveir         ###   ########.fr       */
+/*   Updated: 2025/01/06 10:29:05 by asilveir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./pipex.h"
 
-char	execute_command(char *argv, char **envp)
+void	execute_command(char *argv, char **envp)
 {
 	char	**command;
 	char	*path;
@@ -20,70 +20,71 @@ char	execute_command(char *argv, char **envp)
 	command = ft_split(argv, ' ');
 	if (!command || !command[0])
 	{
-		perror("Invalid command!");
+		perror("Command not found.");
 		exit(EXIT_FAILURE);
 	}
 	path = search_valid_path(command[0], envp);
 	if (!path)
 	{
-		perror("Path error. Invalid command");
-		exit(EXIT_FAILURE);
+		perror("Path error. Command not found.");
+		exit(127);
 	}
 	if (execve(path, command, envp) == -1)
 	{
-		perror("execve failed");
-		exit(EXIT_FAILURE);
+		perror("Execve failed.");
+		exit(127);
 	}
 }
 
-int	child_proccess(int *fd, char **argv, char **envp)
+void	child_proccess(int *fd, char **argv, char **envp)
 {
 	int	in_file;
 
 	in_file = open(argv[1], O_RDONLY, 0644);
 	if (in_file == -1)
 	{
-		perror("Error opening input file!");
-		exit(127);
+		perror("Error opening input file.");
+		exit(EXIT_FAILURE);
 	}
 	if (dup2(fd[1], STDOUT_FILENO) == -1)
 	{
-		perror("dup2 failed");
-		exit(127);
+		perror("Dup2 for fd[1] failed.");
+		exit(EXIT_FAILURE);
 	}
+	close(fd[1]);
 	if (dup2(in_file, STDIN_FILENO) == -1)
 	{
-		perror("dup2 failed");
-		exit(126);
+		perror("Dup2 for in file failed.");
+		exit(EXIT_FAILURE);
 	}
 	close(in_file);
-	close(fd[1]);
+	close(fd[0]);
 	execute_command(argv[2], envp);
 }
 
-int	parent_proccess(int *fd, char **argv, char **envp)
+void	parent_proccess(int *fd, char **argv, char **envp)
 {
 	int	out_file;
 
-	close(fd[1]);
 	out_file = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (out_file == -1)
 	{
-		perror("Error opening output file!");
+		perror("Error opening output file.");
 		exit(EXIT_FAILURE);
 	}
 	if (dup2(fd[0], STDIN_FILENO) == -1)
 	{
-		perror("dup2 failed");
-		exit(EXIT_FAILURE);
+		perror("Dup2 for fd[0] failed.");
+		exit(127);
 	}
+	close(fd[0]);
 	if (dup2(out_file, STDOUT_FILENO) == -1)
 	{
-		perror("dup2 failed");
-		exit(EXIT_FAILURE);
+		perror("Dup2 for fd[1] failed.");
+		exit(127);
 	}
 	close(out_file);
-	close(fd[0]);
+	close(fd[1]);
 	execute_command(argv[3], envp);
 }
 
@@ -100,23 +101,15 @@ int	main(int argc, char *argv[], char **envp)
 {
 	int	fd[2];
 	int	id;
-	int	bytes_read;
+	int	status;
 
 	args_verification(argc);
-	if (!search_valid_path(argv[2], envp))
-	{
-		perror("Error! Command not found");
-		exit(EXIT_FAILURE);
-	}
 	if (pipe(fd) == -1)
-	{
-		perror("Error in pipe!");
 		exit(EXIT_FAILURE);
-	}
 	id = fork();
 	if (id == -1)
 	{
-		perror("Error:\n Fork failed!");
+		perror("Error:\n Fork failed.");
 		exit(EXIT_FAILURE);
 	}
 	if (id == 0)
